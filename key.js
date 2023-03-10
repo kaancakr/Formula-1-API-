03429fa8cfada4328c4b17aeccbf92c16df9e3d7
kaancakr <eren.cakir@tedu.edu.tr>
2023-03-10 17:10:48 +0300

    first commit

---
+++ key.js
@@ -0,0 +1,1 @@
+key = "eb7a48b5b6mshd5677551a4d1571p155995jsn45c54d87ff69";
---
+++ script.js
@@ -0,0 +1,57 @@
+let driverNameRef = document.getElementById("driver-name");
+let searchBtn = document.getElementById("search-btn");
+let result = document.getElementById("result");
+
+//function to fetch data from api
+
+let getMovie = () => {
+    let driverName = driverNameRef.value;
+    let url = `http://ergast.com/api/f1/current/driverStandings=${driverName}`;
+    //if input field is empty
+
+    if (driverName.length <= 0) {
+        result.innerHTML = `<h3 class="msg">Please enter a driver name </h3>`;
+    }
+
+    //if input isn't empty
+    else {
+        fetch(url).then((resp) => resp.json()).then((data) => {
+            //if driver exist in database
+            if (data.Response == "True") {
+                result.innerHTML = `
+                    <div class="info">
+                        <div>
+                            <h2>${data.givenName}</h2>
+                            <h2>${data.familyName}</h2>
+                            <div class="rating">
+                                <img src="star-icon.svg">
+                                <h4>${data.points}</h4>
+                                <h4>${data.positionText}</h4>
+                            </div>
+                            <div class="details">
+                                <span>${data.nationality}</span>
+                                <span>${data.name}</span>
+                                <span>${data.url}</span>
+                            </div>
+                            
+                        </div>
+                    </div>
+                `;
+            }
+
+            //if driver doesn't exist in database
+            else {
+                result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
+            }
+        })
+            //if error occurs
+            .catch(() => {
+                result.innerHTML = `<h3 class="msg">Error Occured</h3>`;
+            });
+    }
+};
+
+
+searchBtn.addEventListener("click", getMovie);
+window.addEventListener("load", getMovie);
+console.log("sa");
